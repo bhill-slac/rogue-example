99730a63468e28348ea71b3287d50cfebc3c9674
Ryan T. Herbst <rherbst@slac.stanford.edu>
2017-01-19 23:19:56 -0800

    RSSI test script.

--- scripts/rssiTest.py
+++ scripts/rssiTest.py
@@ -27,18 +27,43 @@ import rogue.protocols.srp
 import rogue.protocols.udp
 import surf
 import surf.AxiVersion
+import surf.SsiPrbsTx
 import pyrogue.mesh
+import pyrogue.utilities.prbs
 
-link = pyrogue.protocols.UdpRssiPack("192.168.2.126",8192,1500)
-#link = rogue.protocols.udp.Client("192.168.2.126",8193,1500)
+
+class DataRx(rogue.interfaces.stream.Slave):
+
+   def __init__(self):
+      rogue.interfaces.stream.Slave.__init__(self)
+      self.b = 0
+      self.c = 0
+
+   def _acceptFrame(self,frame):
+      self.c += 1
+      self.b += frame.getPayload()
+
+   def getCount(self):
+      return self.c,self.b
+
+
+udpRssiA = pyrogue.protocols.UdpRssiPack("192.168.2.187",8193,1500)
+rssiSrp = rogue.protocols.srp.SrpV3()
+pyrogue.streamConnectBiDir(rssiSrp,udpRssiA.application(0))
+
+udpRssiB = pyrogue.protocols.UdpRssiPack("192.168.2.187",8194,1500)
+#prbsRx = pyrogue.utilities.prbs.PrbsRx('prbsRx')
+#pyrogue.streamConnect(udpRssiB.application(1),prbsRx)
+
+testRx = DataRx()
+pyrogue.streamConnect(udpRssiB.application(1),testRx)
 
 evalBoard = pyrogue.Root('evalBoard','Evaluation Board')
+#evalBoard.add(prbsRx)
 
-srp = rogue.protocols.srp.SrpV0()
-pyrogue.streamConnectBiDir(srp,link.application(0))
-#pyrogue.streamConnectBiDir(srp,link)
-
-evalBoard.add(surf.AxiVersion.create(memBase=srp,offset=0x0))
+evalBoard.add(surf.AxiVersion.create(memBase=rssiSrp,offset=0x00000000))
+evalBoard.add(surf.SsiPrbsTx.create(memBase=rssiSrp,offset=0x0A030000))
+evalBoard.add(surf.Rssi(memBase=rssiSrp,offset=0x0A050000))
 
 # Create mesh node
 mNode = pyrogue.mesh.MeshNode('rogueTest',iface='eth3',root=evalBoard)
@@ -53,10 +78,15 @@ def stop():
 # Start with ipython -i scripts/evalBoard.py
 print("Started")
 
+l = 0
 while(True):
     time.sleep(1)
-    print("Link State: RSSIOpen=%i, DownCount=%i, RssiDropCount=%i, RssiReTrans=%i, PackDropCount=%i" 
-          %
-          (link.getRssiOpen(),link.getRssiDownCount(),link.getRssiDropCount(),
-           link.getRssiRetranCount(),link.getPackDropCount()))
+    print("Link State: RSSIOpen=%i, RSSIBusy=%i, DownCount=%i, RssiDropCount=%i, RssiReTrans=%i, PackDropCount=%i" 
+          % (udpRssiB.getRssiOpen(),udpRssiB.getRssiBusy(),udpRssiB.getRssiDownCount(),udpRssiB.getRssiDropCount(),
+           udpRssiB.getRssiRetranCount(),udpRssiB.getPackDropCount()))
+    c,b = testRx.getCount()
+    w = ((b-l)*8.0) / 1e9
+    l = b
+    print("Rx Count = %i, Rx Bytes = %i, BW = %f" % (c,b,w))
+
 
