01e0a82786152a76a7a34587e6bad5bdd8c3f283
Ryan Herbst <rherbst@slac.stanford.edu>
2017-08-08 17:24:13 -0700

    updates to c++ API

--- src/api_local.cpp
+++ src/api_local.cpp
@@ -5,8 +5,11 @@ int main (int argc, char **argv) {
    try {
       rogue::ApiWrapperPtr wrap = rogue::ApiWrapper::local("evalBoard","EvalBoard");
 
-      printf("Return value int = %li\n",wrap->get("evalBoard.AxiVersion.UpTimeCnt"));
-      printf("Return value str = %s\n",wrap->getDisp("evalBoard.AxiVersion.UpTimeCnt").c_str());
+      printf("Return value int = %li\n",wrap->getUInt32("evalBoard.AxiVersion.UpTimeCnt"));
+      printf("Return value str = %s\n",wrap->getString("evalBoard.AxiVersion.UpTimeCnt").c_str());
+
+      wrap->setUInt32("evalBoard.AxiVersion.ScratchPad",0xCC);
+      printf("Spad value int = 0x%x\n",wrap->getUInt32("evalBoard.AxiVersion.ScratchPad"));
 
       Py_BEGIN_ALLOW_THREADS;
       while(1) {
--- src/api_remote.cpp
+++ src/api_remote.cpp
@@ -5,8 +5,11 @@ int main (int argc, char **argv) {
    try {
       rogue::ApiWrapperPtr wrap = rogue::ApiWrapper::remote("rogueTest","evalBoard");
 
-      printf("Return value int = %li\n",wrap->get("evalBoard.AxiVersion.UpTimeCnt"));
-      printf("Return value str = %s\n",wrap->getDisp("evalBoard.AxiVersion.UpTimeCnt").c_str());
+      printf("Return value int = %li\n",wrap->getUInt32("evalBoard.AxiVersion.UpTimeCnt"));
+      printf("Return value str = %s\n",wrap->getString("evalBoard.AxiVersion.UpTimeCnt").c_str());
+
+      wrap->setUInt32("evalBoard.AxiVersion.ScratchPad",0xCC);
+      printf("Spad value int = 0x%x\n",wrap->getUInt32("evalBoard.AxiVersion.ScratchPad"));
 
    } catch (boost::python::error_already_set) {
       PyErr_Print();
