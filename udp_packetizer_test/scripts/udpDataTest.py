9a979c66b81c2f7aa1ff104c7d1a4d0d1920d6c2
Ryan Herbst <rherbst@slac.stanford.edu>
2018-03-03 20:45:39 -0800

    Updates

--- udp_packetizer_test/scripts/udpDataTest.py
+++ udp_packetizer_test/scripts/udpDataTest.py
@@ -19,43 +19,41 @@ import rogue.interfaces.stream
 import pyrogue
 import time
 
+#rogue.Logging.setLevel(rogue.Logging.Debug)
+
 # Server chain
-serv = rogue.protocols.udp.Server(0,1024);
+serv = rogue.protocols.udp.Server(0,1500);
 port = serv.getPort()
 print("Port is {}".format(port))
 
-#sRssi = rogue.protocols.rssi.Server(1024)
-#pyrogue.streamConnectBiDir(serv,sRssi.transport())
+sRssi = rogue.protocols.rssi.Server(1024)
+pyrogue.streamConnectBiDir(serv,sRssi.transport())
 
-#sPack = rogue.protocols.packetizer.Core(1024)
-#pyrogue.streamConnectBiDir(sRssi.application(),sPack.transport())
+sPack = rogue.protocols.packetizer.Core(1024)
+pyrogue.streamConnectBiDir(sRssi.application(),sPack.transport())
 
 prbsRx = rogue.utilities.Prbs()
-pyrogue.streamConnect(serv,prbsRx)
-#pyrogue.streamConnect(sPack.application(0),prbsRx)
+pyrogue.streamConnect(sPack.application(0),prbsRx)
 
 # Client chain
-client = rogue.protocols.udp.Client("localhost",port,1024);
+client = rogue.protocols.udp.Client("localhost",port,1500);
 
-#cRssi = rogue.protocols.rssi.Client(1024)
-#pyrogue.streamConnectBiDir(client,sRssi.transport())
+cRssi = rogue.protocols.rssi.Client(1024)
+pyrogue.streamConnectBiDir(client,cRssi.transport())
 
-#cPack = rogue.protocols.packetizer.Core(1024)
-#pyrogue.streamConnectBiDir(cRssi.application(),cPack.transport())
+cPack = rogue.protocols.packetizer.Core(1024)
+pyrogue.streamConnectBiDir(cRssi.application(),cPack.transport())
 
 prbsTx = rogue.utilities.Prbs()
-pyrogue.streamConnect(prbsTx,client)
-#pyrogue.streamConnect(prbsTx,cPack.application(0))
-
-#pyrogue.streamConnectBiDir(sRssi.transport(),cRssi.transport())
+pyrogue.streamConnect(prbsTx,cPack.application(0))
 
 # Enable 
-prbsTx.enable(1000)
+prbsTx.enable(20000)
 
 while (True):
 
    print("")
-   print(" Source: Count {}, Bytes {}".format(prbsTx.getTxCount(),prbsTx.getTxBytes()))
-   print(" Dest:   Count {}, Bytes {}, Errors {}".format(prbsRx.getRxCount(),prbsRx.getRxBytes(),prbsRx.getRxErrors()))
+   print(" Source: Open {}, Count {}, Bytes {}".format(cRssi.getOpen(),prbsTx.getTxCount(),prbsTx.getTxBytes()))
+   print(" Dest:   Open {}, Count {}, Bytes {}, Errors {}".format(sRssi.getOpen(),prbsRx.getRxCount(),prbsRx.getRxBytes(),prbsRx.getRxErrors()))
    time.sleep(1)
 
