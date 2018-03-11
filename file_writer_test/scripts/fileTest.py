fb760fd3f5f6074322aaceed3a39380a3a7a66aa
Ryan Herbst <rherbst@slac.stanford.edu>
2018-03-10 20:54:17 -0800

    Update

--- file_writer_test/scripts/fileTest.py
+++ file_writer_test/scripts/fileTest.py
@@ -29,10 +29,7 @@ fwr.setBufferSize(100004)
 fwr.setMaxSize(1000003)
 
 prbsA = rogue.utilities.Prbs()
-#compA = rogue.utilities.StreamZip()
 
-#pyrogue.streamConnect(prbsA,compA)
-#pyrogue.streamConnect(compA,fwr.getChannel(0x5))
 pyrogue.streamConnect(prbsA,fwr.getChannel(0x5))
 
 fwr.open("test.dat")
@@ -50,11 +47,8 @@ print("     File: Count %i, Bytes %i" % (fwr.getFrameCount(),fwr.getSize()))
 
 frd = rogue.utilities.fileio.StreamReader()
 
-#compB = rogue.utilities.StreamUnZip()
 prbsB = rogue.utilities.Prbs()
 
-#pyrogue.streamConnect(frd,compB)
-#pyrogue.streamConnect(compB,prbsB)
 pyrogue.streamConnect(frd,prbsB)
 
 frd.open("test.dat.1")
